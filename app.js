e80489b8fe050accd79a4d79684e06209403d519
Don Cruickshank <dgc@soton.ac.uk>
2014-12-17 15:39:33 +0000

    Added breadcrumbs to error pages.

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
+            breadcrumbs: [ { label: 'Home', uri: '/' } ],
             message: err.message,
             error: err
         });
@@ -55,6 +56,7 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
+        breadcrumbs: [ { label: 'Home', uri: '/' } ],
         message: err.message,
         error: {}
     });
